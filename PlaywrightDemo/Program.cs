42350338fe8494de7428b514fefd0eb01fcf160f
tilston1000 <info@stratosphereqa.com>
2026-06-07 23:53:10 +0100

    commit 2

---
+++ PlaywrightDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Playwright;
+
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Firefox.LaunchAsync(new()
+{
+    Headless = false,
+    SlowMo = 50
+});
+var page = await browser.NewPageAsync();
+await page.GotoAsync("https://playwright.dev/dotnet");
+await page.ScreenshotAsync(new()
+{
+    Path = "screenshot.png"
+});
